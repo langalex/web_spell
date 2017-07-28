1268e94513db7b322337dadf9f216c6a3a0301b9
Alexander Lang <alex@upstre.am>
2017-07-28 17:35:18 +0200

    adds ex_doc

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule WebSpell.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
   defp description do
