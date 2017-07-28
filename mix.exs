9b3dd41c93712aea0edd0695a1c3481aed829eb0
Alexander Lang <alex@upstre.am>
2017-07-28 17:32:09 +0200

    adds project metadata, license

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule WebSpell.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package(),
+     name: "WebSpell",
+     source_url: "https://github.com/langalex/web_spell"]
   end
 
   # Configuration for the OTP application
@@ -15,7 +19,7 @@ defmodule WebSpell.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: []]
   end
 
   # Dependencies can be Hex packages:
@@ -29,5 +33,22 @@ defmodule WebSpell.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+      WebSpell is an HTTP mocking library for Elixir. It is somewhat inspired by WebMock for Ruby, but adapted to the different programming environment of Elixir.
+    """
+  end
+
+  defp package do
+    # These are the default files included in the package
+    [
+      name: :web_spell,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Alexander Lang"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/langalex/web_spell"}
+    ]
   end
 end
